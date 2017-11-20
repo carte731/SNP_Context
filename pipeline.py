c308808ac358f416001fb428afbecc8313913791
carte731 <carte731@umn.edu>
2017-11-20 10:53:54 -0600

    More check stuff

--- pipeline.py
+++ pipeline.py
@@ -11,7 +11,7 @@ import logging
 
 def initModLoad():
     vCheck = sys.version_info[0:3]
-    if(vCheck != 3.6.1):
+    if(vCheck != (3,6,1)):
         print("Please load python 3.6.1")
     # subprocess.call("python3_ML/3.6.0")
     subprocess.call("module load bedops_ML/2.4.20")
