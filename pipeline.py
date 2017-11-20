820458657173969ff558b6e01f9819de9d7eff29
carte731 <carte731@umn.edu>
2017-11-20 11:26:38 -0600

    version testing

--- pipeline.py
+++ pipeline.py
@@ -21,8 +21,8 @@ def pythonVersionCheck():
         if(lib.find_loader('mutation_motif') == None):
                 return("\nPlease install mutation motif module or module load the Morrell version of python 3.6.1\n")
     elif(sys.version_info[0] == 2):
-        import lib
-        if(lib.find_loader('mutation_motif') == None):
+        import imp as lib
+        if(lib.find_module('mutation_motif') == None):
                 return("\nPlease install mutation motif module or module load the Morrell version of python 3.6.1\n")
 
 def start():
